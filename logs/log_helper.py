593eaf184ee1478c66faa485e91c77920f1b122b
JamesKen <41173585+Jamesken23@users.noreply.github.com>
2023-07-11 15:32:11 +0800

    Add files via upload

---
+++ logs/log_helper.py
@@ -0,0 +1,31 @@
+import logging, time, os
+
+def get_log_path(args):
+    # 创建日志文件
+    if not os.path.exists(args.log_dir):
+        os.mkdir(args.log_dir)
+    # 获取当前时间作为日志文件名称
+    current_time=time.strftime('%Y%m%d%H%M',time.localtime(time.time() )) 
+    
+    log_txt_name = args.model_name + "_" + args.sc_vul_type + "_" + current_time +".txt"
+    log_txt_path = os.path.join(args.log_dir, log_txt_name)
+    return log_txt_path, log_txt_name
+
+
+def get_logger(filename, verbosity=1, name=None):
+    level_dict = {0: logging.DEBUG, 1: logging.INFO, 2: logging.WARNING}
+    formatter = logging.Formatter(
+        "[%(asctime)s][%(filename)s][line:%(lineno)d][%(levelname)s] %(message)s"
+    )
+    logger = logging.getLogger(name)
+    logger.setLevel(level_dict[verbosity])
+
+    fh = logging.FileHandler(filename, "w")
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
+
+    sh = logging.StreamHandler()
+    sh.setFormatter(formatter)
+    logger.addHandler(sh)
+
+    return logger
